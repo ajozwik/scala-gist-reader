7e17745c1678d314ea523dbdfc075e7535778fcd
andrzej.jozwik@gmail.com <jolka13>
2013-12-06 23:08:58 +0100

    scalania is not hardcoded now

--- testRunner/src/main/scala/pl/jozwik/scalania/TestRunner.scala
+++ testRunner/src/main/scala/pl/jozwik/scalania/TestRunner.scala
@@ -11,31 +11,42 @@ import java.net.URL
 
 object TestRunner {
 
+  val gitPostfix = ".git"
+
   def main(args: Array[String]) {
     val packageName = args(0)
     val objectName = args(1)
     val signature = args(2)
     val testName = args(3)
 
-    uploadSolutionsAndRunTests(new URL("https://github.com/jaceklaskowski/scalania.git"),"exercises",packageName, objectName, signature, testName, Seq(7680647, 7680700))
+    uploadSolutionsAndRunTests(new URL("https://github.com/jaceklaskowski/scalania.git"), "exercises", packageName, objectName, signature, testName, Seq(7680647, 7680700))
   }
 
 
-  def uploadSolutionsAndRunTests(repositoryUrl:URL,subProject:String,packageName: String, objectName: String, signature: String, testName: String, numbers: Seq[Int], url: String = GistReader.DEFAULT_URL): (Seq[String],String) = {
+  def extractName(url: String): String = {
+    val lastSlash = url.lastIndexOf('/')
+    val name = url.substring(lastSlash+1)
+    if (name.endsWith(gitPostfix)) {
+      name.substring(0, name.length - gitPostfix.length)
+    } else {
+      name
+    }
+  }
+
+  def uploadSolutionsAndRunTests(repositoryUrl: URL, subProject: String, packageName: String, objectName: String, signature: String, testName: String, numbers: Seq[Int], url: String = GistReader.DEFAULT_URL): (Seq[String], String) = {
     val objectContent = gistsToFile(packageName, objectName, signature, numbers, url)
-
-
+    val repoDir = extractName(repositoryUrl.toString)
     val tmpDir = new File("tmp")
-    val scalaniaDir = new File(tmpDir, "scalania")
+    val scalaniaDir = new File(tmpDir, repoDir)
 
     cloneRepository(scalaniaDir, repositoryUrl)
 
-    storeFileWithTests(scalaniaDir,subProject, packageName, objectName, objectContent)
+    storeFileWithTests(scalaniaDir, subProject, packageName, objectName, objectContent)
 
-    (runSbt(packageName, testName, scalaniaDir),objectContent)
+    (runSbt(packageName, testName, scalaniaDir), objectContent)
   }
 
-  private def runSbt(packageName: String, testName: String, scalaniaDir: File):Seq[String] = {
+  private def runSbt(packageName: String, testName: String, scalaniaDir: File): Seq[String] = {
     val sbtPb = Process(Seq("sbt", "testOnly " + packageName + s".$testName", ""), scalaniaDir)
     println(s"$sbtPb")
 
@@ -48,8 +59,9 @@ object TestRunner {
     builder.toSeq
   }
 
-  private def storeFileWithTests(scalaniaDir: File,subProject:String, packageName: String, objectName: String, content: String) {
-    val srcPath = FileSystems.getDefault.getPath(scalaniaDir.getAbsolutePath, subProject, "src", "main", "scala")
+  private def storeFileWithTests(scalaniaDir: File, subProject: String, packageName: String, objectName: String, content: String) {
+    val sub = if (subProject.isEmpty) "." else subProject
+    val srcPath = FileSystems.getDefault.getPath(scalaniaDir.getAbsolutePath, sub, "src", "main", "scala")
     val splitted = packageName.split("\\.")
     val packageDir = splitted.foldLeft(srcPath.toFile)((f, str) => new File(f, str))
     val location = new File(packageDir, objectName + ".scala")
---
+++ testRunner/src/test/scala/pl/jozwik/scalania/TestRunnerTest.scala
@@ -0,0 +1,15 @@
+package pl.jozwik.scalania
+
+import org.specs2.mutable
+
+class TestRunnerTest extends mutable.Specification {
+  "TestRunner " should {
+    "Extract repository name from url " in {
+      val expected = "repository"
+      TestRunner.extractName("https://paos/posda/" + expected) === expected
+      val expectedPosfixGit = expected + TestRunner.gitPostfix
+      TestRunner.extractName("https://paos/posda/" + expectedPosfixGit) === expected
+    }
+  }
+
+}
